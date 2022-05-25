0b0fc7c3018ff49feda9eda2e7d93a91e0c0e118
pmogan77 <60144163+pmogan77@users.noreply.github.com>
2022-05-24 22:52:12 -0500

    test proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://0.0.0.0:"+process.env.PORT,
+      target: "http://0.0.0.0"+process.send.PORT,
       changeOrigin: true,
     })
   );
